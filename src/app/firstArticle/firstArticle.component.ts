8ee5044cb0c58444e10f439eff44b1bb2d006f1e
gheorghecr <gheorghecr1997@gmail.com>
2021-04-27 15:59:07 +0100

    Adding new First Article page

---
+++ src/app/firstArticle/firstArticle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-firstArticle',
+  templateUrl: './firstArticle.component.html',
+  styleUrls: ['./firstArticle.component.css']
+})
+export class FirstArticleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
