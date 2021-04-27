bf67f6165e6890298cd8308235c3c9f8ee936ac1
gheorghecr <gheorghecr1997@gmail.com>
2021-04-27 18:05:35 +0100

    Adding click event to buttons

--- src/app/firstArticle/firstArticle.component.ts
+++ src/app/firstArticle/firstArticle.component.ts
@@ -12,4 +12,25 @@ export class FirstArticleComponent implements OnInit {
   ngOnInit() {
   }
 
+  /**
+   * Download Button Clicked
+   */
+   downloadDocClicked() {
+    console.log("downloadDocClicked")
+  }
+
+  /**
+   * Previous Document version Clicked
+   */
+   previousVersionClicked() {
+    console.log("previousVersionClicked")
+  }
+
+  /**
+   * Next Document version Button Clicked
+   */
+   nextVersionClicked() {
+    console.log("nextVersionClicked")
+  }
+
 }
