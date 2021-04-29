e55a6d50421bb00f81e7fb8b714ea2fa383c3dcf
gheorghecr <gheorghecr1997@gmail.com>
2021-04-29 16:29:06 +0100

    Adding the pdf views from and URL in the class, it was necessary to have the URL Sanitazer

--- src/app/firstArticle/firstArticle.component.ts
+++ src/app/firstArticle/firstArticle.component.ts
@@ -1,25 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { SafeResourceUrl } from '@angular/platform-browser';
+import { DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-firstArticle',
   templateUrl: './firstArticle.component.html',
-  styleUrls: ['./firstArticle.component.css']
+  styleUrls: ['./firstArticle.component.css'],
 })
 export class FirstArticleComponent implements OnInit {
-
   fileNameToDisplay = 'File Name Here';
   secondaryFileNameToDisplay = 'Secondary File Name Here';
 
   fileMetaData = [
-    { key: 'Created by:', value: 'Gheorghe'},
-    { key: 'Last Modified:', value: '21/02/2021'},
-    { key: 'Created by:', value: 'Gheorghe2'},
-    { key: 'Last Modified:', value: '22/02/2021'},
-    { key: 'Created by:', value: 'Gheorghe3'},
-    { key: 'Last Modified:', value: '23/02/2021'},
-];
+    { key: 'Created by:', value: 'Gheorghe' },
+    { key: 'Last Modified:', value: '21/02/2021' },
+    { key: 'Created by:', value: 'Gheorghe2' },
+    { key: 'Last Modified:', value: '22/02/2021' },
+    { key: 'Created by:', value: 'Gheorghe3' },
+    { key: 'Last Modified:', value: '23/02/2021' },
+  ];
 
-  constructor() { }
+  pdfFileNameToPresent = './assets/Docs/manual5.pdf#toolbar=0';
+  pdfFileNameToDownload = './assets/Docs/manual5.pdf';
+
+  urlFileToPresent: SafeResourceUrl;
+
+  constructor(sanitizer: DomSanitizer) {
+    this.urlFileToPresent = sanitizer.bypassSecurityTrustResourceUrl(this.pdfFileNameToPresent);
+  }
 
   ngOnInit() {
   }
@@ -27,22 +35,26 @@ export class FirstArticleComponent implements OnInit {
   /**
    * Download Button Clicked
    */
-   downloadDocClicked() {
-    console.log("downloadDocClicked")
+  downloadDocClicked() {
+    let link = document.createElement('a');
+    link.setAttribute('type', 'hidden');
+    link.href = this.pdfFileNameToDownload;
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
   }
 
   /**
    * Previous Document version Clicked
    */
-   previousVersionClicked() {
-    console.log("previousVersionClicked")
+  previousVersionClicked() {
+    console.log('previousVersionClicked');
   }
 
   /**
    * Next Document version Button Clicked
    */
-   nextVersionClicked() {
-    console.log("nextVersionClicked")
+  nextVersionClicked() {
+    console.log('nextVersionClicked');
   }
-
 }
