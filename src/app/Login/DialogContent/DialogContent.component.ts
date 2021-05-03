2abadfa336972d42038f2bcf79d2f8d302e66889
gheorghecr <gheorghecr1997@gmail.com>
2021-05-03 10:48:46 +0100

    Adding more documentation

--- src/app/Login/DialogContent/DialogContent.component.ts
+++ src/app/Login/DialogContent/DialogContent.component.ts
@@ -2,15 +2,15 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { LoginData } from '../Login/LoginData';
 
-/**
- * This is the component that is responsible for the modal view.
- * It's only used for the modal.
- */
 @Component({
   selector: 'app-DialogContent',
   templateUrl: './DialogContent.component.html',
   styleUrls: ['./DialogContent.component.css'],
 })
+
+/**
+ * This is the component that is responsible for the login modal view.
+ */
 export class DialogContentComponent implements OnInit {
   constructor(
     public dialogRef: MatDialogRef<DialogContentComponent>,
