a9c0a845919669e3073d2b7d426d9dc1ad382276
gheorghecr <gheorghecr1997@gmail.com>
2021-05-18 14:37:39 +0100

    Adding linkButtons component

---
+++ src/app/linkButtons/linkButtons/linkButtons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-linkButtons',
+  templateUrl: './linkButtons.component.html',
+  styleUrls: ['./linkButtons.component.css']
+})
+export class LinkButtonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
