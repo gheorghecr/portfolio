9e0c8a10f44554227580066d82f5162b4193a528
Gheorghe Craciun <gheorghecr1997@gmail.com>
2021-05-26 12:01:39 +0100

    Adding projects data

---
+++ src/app/projects/projects/projectsList/projects.ts
@@ -0,0 +1,27 @@
+export interface Projects {
+    id: number;
+    title: string;
+    subtitle: string;
+    technologies: string;
+  }
+  
+  export const projects = [
+    {
+      id: 1,
+      title: 'Phone XL',
+      subtitle: 'Subtitle',
+      technologies: 'A large phone with one of the best screens'
+    },
+    {
+      id: 2,
+      title: 'Phone Mini',
+      subtitle: 'Subtitle',
+      technologies: 'A great phone with one of the best cameras'
+    },
+    {
+      id: 3,
+      title: 'Phone Standard',
+      subtitle: 'Subtitle',
+      technologies: 'A great phone with one of the best cameras'
+    }
+  ];
