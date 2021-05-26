8080bde324040c6bd8c1678c9bb664cafdff65da
Gheorghe Craciun <gheorghecr1997@gmail.com>
2021-05-26 16:39:50 +0100

    Adding animation slide In to projects divs

--- src/app/projects/projects/projects.component.ts
+++ src/app/projects/projects/projects.component.ts
@@ -1,10 +1,26 @@
 import { Component, OnInit, Injectable } from '@angular/core';
 import { projects, Projects } from './projectsList/projects';
 
+import { trigger, transition, animate, style } from '@angular/animations'
+
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
+  animations: [
+    trigger('slideLeftToRight', [
+      transition(':enter', [
+        style({transform: 'translateX(-100%)', opacity: 0.2}),
+        animate('1000ms ease-in', style({transform: 'translateX(0%)', opacity: 1}))
+      ]),
+    ]),
+    trigger('slideRightToLeft', [
+      transition(':enter', [
+        style({transform: 'translateX(100%)', opacity: 0.2}),
+        animate('1000ms ease-in', style({transform: 'translateX(0%)', opacity: 1}))
+      ]),
+    ])
+  ]
 })
 
 @Injectable({
